975e433933aa37be9fd5c2bb61345ef10850367d
Artur Carneiro <carneiro@Arturs-MacBook-Pro.local>
2019-05-29 21:08:38 -0300

    Bronze and Prata done

---
+++ CustomView/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Helper.swift
+//  CustomView
+//
+//  Created by Artur Carneiro on 29/05/19.
+//  Copyright © 2019 Joaquim Pessoa Filho. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum ColorPallete {
+    static let red = #colorLiteral(red: 0.9764705882, green: 0.3411764706, blue: 0.2196078431, alpha: 1)
+    static let orange = #colorLiteral(red: 0.9764705882, green: 0.5333333333, blue: 0.2117647059, alpha: 1)
+    static let yellow = #colorLiteral(red: 0.9764705882, green: 0.7607843137, blue: 0.2117647059, alpha: 1)
+    static let green = #colorLiteral(red: 0.737254902, green: 0.9764705882, blue: 0.2117647059, alpha: 1)
+    static let blue = #colorLiteral(red: 0.2117647059, green: 0.9764705882, blue: 0.9254901961, alpha: 1)
+    static let indigo = #colorLiteral(red: 0.2117647059, green: 0.568627451, blue: 0.9764705882, alpha: 1)
+    static let violet = #colorLiteral(red: 0.5843137255, green: 0.2117647059, blue: 0.9764705882, alpha: 1)
+}
